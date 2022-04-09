d12bed21d4e851c92b259d200b9966f9ed19176c
ndu <ndu>
2022-04-09 23:48:27 +0300

    updated historical data manager

--- data_manager.py
+++ data_manager.py
@@ -1,11 +1,14 @@
 from binance import Client
 import user_api_key
 import csv
-import winsound
 from tqdm import tqdm
 
-duration = 1000  # milliseconds
-freq = 440  # Hz
+""" 
+ ***    Data List   ***
+[Open time, Open, High, Low, Close, Volume, Close time, 
+ Quote asset volume, Number of trades, Taker buy base asset volume, 
+ Taker buy quote asset volume, Ignore]
+"""
 
 """
     KLINE_INTERVAL_1MINUTE = '1m'
@@ -24,12 +27,14 @@ freq = 440  # Hz
     KLINE_INTERVAL_1WEEK = '1w'
     KLINE_INTERVAL_1MONTH = '1M'
 """
-client = Client(user_api_key.key_id, user_api_key.secret_key_id)
-symbolList = ["BTCUSDT"]
-timeFrame= "1w"
 
-startDateOfData = "12 March, 2020"
-endDateOfData = "12 March, 2022"
+client = Client(user_api_key.key_id, user_api_key.secret_key_id)
+#symbolList = ["BTCUSDT", "ETHUSDT", "ADAUSDT", "SOLUSDT", "AVAXUSDT", "NEARUSDT"]
+symbolList = ["AVAXUSDT", "NEARUSDT"]
+timeFrame= client.KLINE_INTERVAL_3MINUTE
+
+startDateOfData = "1 January, 2020"
+endDateOfData = "9 April, 2022"
 
 def historical_data_write_to_file():
     csvFileW = open(symbol + "_" + timeFrame +".csv", "w", newline='')
@@ -42,8 +47,8 @@ def historical_data_write_to_file():
 
 # Get Historical Data
 for symbol in tqdm(symbolList):
-    print("Data Downloading...: " + symbol + " " + timeFrame + " Time Frame")
+    print("\nStarted Data Downloading...: " + symbol + " " + timeFrame + " Time Frame")
     candlesticks = client.get_historical_klines(symbol, timeFrame, startDateOfData, endDateOfData)
     historical_data_write_to_file()
 
-winsound.Beep(freq, duration)
+print("Finished Data Downloading...")
