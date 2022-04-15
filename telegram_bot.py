5d32c6aab17a49104b056445540b70b519e5f9b8
ndu <ndu>
2022-04-15 21:23:02 +0300

    updated bot id and channel id for messaging

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,21 +1,27 @@
 from datetime import datetime 
 import requests 
+import tkinter 
+from tkinter import *
 from threading import Thread 
 
 
-# api = 5178648263:AAESJwnZBqv-C8IGRwFK4MJ7UgD93Yghe6I  
-#https://api.telegram.org/bot5178648263:AAESJwnZBqv-C8IGRwFK4MJ7UgD93Yghe6I/sendMessage 
-#-593936905 grup 
-#1122622679 ibrahim 
+        
+# api = 5356826126:AAEjHzEKvwhFDoy4wdnDdtK9dtxTz8vN94c  
 
-mesaj_url = "https://api.telegram.org/bot5178648263:AAESJwnZBqv-C8IGRwFK4MJ7UgD93Yghe6I/sendMessage" 
+# Kontrol et aktifse true 
+# https://api.telegram.org/bot5356826126:AAEjHzEKvwhFDoy4wdnDdtK9dtxTz8vN94c 
+
+# Bot ID    : 593917120
+# Grup ID   : -1001542109604
+ 
+message_url = "https://api.telegram.org/bot5356826126:AAEjHzEKvwhFDoy4wdnDdtK9dtxTz8vN94c/sendMessage"
 
 def send_message(message): 
     #iş yükü parçacıgı için 
     def thread1(): 
         # mesajı telegrama yollayalım 
-        requests.post(url=mesaj_url ,data={"chat_id":"-593936905","text":message}).json()                
+        requests.post(url=message_url ,data={"chat_id":"-1001542109604","text":message}).json()                
  
     #thread ile fonksiyonu başlatır 
     th = Thread(target=thread1) 
-    th.start() 
+    th.start()
