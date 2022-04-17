5025768bce60e5f486ac66e29a109a5b7070fc19
ndu <ndu>
2022-04-17 03:33:29 +0300

    updated fibonacci retracement

--- Indicators/fibonacci_retracement.py
+++ Indicators/fibonacci_retracement.py
@@ -9,12 +9,9 @@ FIB_0_500 = []
 FIB_0_382 = []
 FIB_0_236 = []
 FIB_0 = []
+FIB = []
 
-#csvName = "Historical_Data/BTCUSDT_1d.csv"
-
-#attributes = ["openTime","open","high","low","close","volume","closeTime","2","3","4","5","6"]
-#data = pd.read_csv(csvName, names = attributes)
-
+'''
 def calculate_fib(df, period): 
     global counter
 
@@ -37,13 +34,34 @@ def calculate_fib(df, period):
             counter = counter + 1
         else: 
             break     
-    
+     
     df['FIB_1'] = pd.Series(FIB_1) 
     df['FIB_0_500'] = pd.Series(FIB_0_500)
-    df['FIB_0'] = pd.Series(FIB_0)
-    df["openTime"] = pd.to_datetime(df["openTime"],unit= "ms")
-    df["closeTime"] = pd.to_datetime(df["closeTime"],unit= "ms")
+    df['FIB_0'] = pd.Series(FIB_0)    
+'''  
+
+def calculate_fib(df, period, fibVal): 
+    global counter
+    FIB = []
+    counter = 0
     
+    for k in range (period):
+        FIB.append('NaN')       
+        k = k + 1
+
+    for i in df['close']:
+        if (counter + period) < len(df): 
+            df2 = df.iloc[counter : counter + period, : ]
+            maxr = df2['high'].max()
+            minr = df2['low'].min()
+            ranr = maxr - minr 
+            
+            FIB.append(minr + fibVal * ranr)
+            counter = counter + 1
+        else: 
+            break     
+     
+    return pd.Series(FIB) 
 
 #calculate_fib(data, 10)
 #print(data)
