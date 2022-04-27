bc72a56a5fa3179415ddb9d2edb6198e177c0611
ndu <ndu>
2022-04-27 23:42:58 +0300

    updated tarama with thread and code refinement

--- tmt_tarama.py
+++ tmt_tarama.py
@@ -1,97 +1,26 @@
-from operator import index
-import array as arr
-from binance.client import Client  
-import pandas as pd 
-import pandas_ta as tb
-import user_api_key 
-import time   
-from telegram_bot import *
+from time import sleep   
+from telegram_bot import warn, send_message_to_telegram, channelTarama
 from datetime import datetime 
-from datetime import timedelta
-from ta.momentum import RSIIndicator
-from data_manager import get_symbol_list
-import operator
-
-client = Client(user_api_key.key_id,user_api_key.secret_key_id) 
-
-# Teknik Analiz
-rsiPeriod = 21
-
-# Parite Bilgileri
-timeFrame = 1
-interval = '1m' 
-symbol = ""
-limit = rsiPeriod * 6
+from data_manager import get_symbol_list, get_calculated_hype_symbol_list
+from threading import Thread
 
 symbolListFuture = get_symbol_list("USDT", "Future")
 symbolListSpot = get_symbol_list("USDT", "Spot")
-symbolList = ""
 
-debugMsg = ""
-candleTime = 0
-hypeRate = 0
-searchList = {}
-
-def calculate_hype_point(market):
-    global symbolList
+def do_work_hype_coin_scanning(market): 
     global symbolListFuture
     global symbolListSpot
-    global searchList
-    global candleTime
+    interval = '1m' 
+    symbolList = ""
+    candleTime = ""
+    searchList = {}
 
-    if (market == "Spot"):
-        symbolList = symbolListSpot
-    elif (market == "Future"):
-        symbolList = symbolListFuture
+    if (market == "Future"):
+        symbolList = symbolListFuture  
+    elif (market == "Spot"):
+        symbolList = symbolListSpot  
 
-    for symbol in symbolList:
-        if (market == "Spot"):
-            candles = client.get_klines(symbol=symbol, interval=interval, limit=limit) 
-        elif (market == "Future"):
-            candles = client.futures_klines(symbol=symbol, interval=interval, limit=limit)
-
-        ## Get Data
-        df = pd.DataFrame(candles, columns=['openTime', 'open', 'high', 'low', 'close', 'volume', 'closeTime', 
-                                            'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 
-                                            'taker_buy_quote_asset_volume', 'ignore']) 
-        
-        ## Clean data 
-        df = df[['openTime', 'open', 'high', 'low', 'close', 'closeTime']]       
-        df['openTime'] = pd.to_datetime(df["openTime"], unit="ms") + timedelta(hours=3)
-        df['closeTime'] = pd.to_datetime(df["closeTime"], unit="ms") + timedelta(hours=3)
-        df['high'] = df['high'].astype('float')
-        df['open'] = df['open'].astype('float') 
-        df['close'] = df['close'].astype('float')          
-        df['low'] = df['low'].astype('float')
-        df["RSI"] = tb.rsi(df["close"],rsiPeriod)
-        
-        ## Calculate Hype Rate
-        candleTime = df['openTime'][limit-2]
-        hypeRate = (df['high'][limit-2] / df['low'][limit-2]) + abs(df['RSI'][limit-2] - 35)
-        #hypeRate = (df['high'][limit-2] / df['low'][limit-2])
-        #hypeRate = (df['high'][limit-2] / df['low'][limit-2]) + abs(df['RSI'][limit-2] - 50) # RSI kapatıldı
-        hypeRate = round(hypeRate,5)
-
-        ## Add to hype list
-        if hypeRate >= 0:
-            searchList[symbol] = hypeRate
-
-    ## Sort from biggest to smallest
-    searchList = dict(sorted(searchList.items(),key=operator.itemgetter(1),reverse = True)) # ascending order   
-
-    # Update symbol lists
-    symbolListFuture = get_symbol_list("USDT", "Future")
-    symbolListSpot = get_symbol_list("USDT", "Spot")
-
-def do_work_hype_coin_scanning(market):
-    # Aynı mum içinde tekrar tekrar işlem yapmasın diye bir sonraki mum açılışını bekle
-    # Wait 1 second until we are synced up with the 'every 5 minute' clock  
-    #while datetime.now().minute not in {0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55}: 
-    #while datetime.now().minute % timeFrame != 0: 
-    while datetime.now().second == 0:
-        time.sleep(1) 
-
-    calculate_hype_point(market)
+    searchList, candleTime  = get_calculated_hype_symbol_list(market, interval, symbolList)
 
     debugMsg = ""
     debugMsg = warn + " " + market + " Coin Liste Taraması (" + interval + ")\n\n"
@@ -107,6 +36,14 @@ def do_work_hype_coin_scanning(market):
     send_message_to_telegram(channelTarama, debugMsg)
     debugMsg = ""
 
+    if(datetime.now().minute == 0):
+        if (market == "Future"):
+            symbolListFuture = get_symbol_list("USDT", "Future")
+        elif (market == "Spot"):
+            symbolListSpot = get_symbol_list("USDT", "Spot")   
+
 while (1):
-    do_work_hype_coin_scanning("Future")    
-    time.sleep(1)        
+    if (datetime.now().second == 1):
+        t = Thread(target=do_work_hype_coin_scanning, args=["Future"])
+        t.start()
+    sleep(1)        
