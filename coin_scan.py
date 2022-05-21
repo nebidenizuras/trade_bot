bf41d3d0e8de01ae10349e0bac47de4383ecc3ca
ndu <ndu>
2022-05-21 18:00:59 +0300

    updated coin scan while using thread

--- coin_scan.py
+++ coin_scan.py
@@ -98,7 +98,7 @@ def compute_process(timeFrame):
 
     send_message(longMsg + "\n" + shortMsg)
 
-
+IsTime5m = False
 IsTime15m = False
 IsTime1h = False
 IsTime4h = False
@@ -108,6 +108,91 @@ dateTime = datetime.now()
 
 send_message(warn + "\nTarama Başlatıldı...\nSaat : " + str(dateTime.hour) + ":" + str(dateTime.minute) + "\n" + warn)
 symbol_list = get_symbol_list('USDT','Future')
+
+t5m = Thread(target=compute_process, args=["5m"])
+t15m = Thread(target=compute_process, args=["15m"])
+t1h = Thread(target=compute_process, args=["1h"])
+t4h = Thread(target=compute_process, args=["4h"])
+t1d = Thread(target=compute_process, args=["1d"])
+
+while True:   
+    dateTime = datetime.now()
+
+    if (dateTime.minute % 5 != 0):
+        IsOK5m = False
+    if (dateTime.minute % 15 != 0):
+        IsOK15m = False
+    if (dateTime.minute != 0):
+        IsOK1h = False
+        IsOK4h = False
+    if (dateTime.hour != 3):
+        IsOK1d = False
+
+    if (dateTime.minute % 5 == 0) and (IsOK5m == False): 
+        t5m = Thread(target=compute_process, args=["5m"])
+        t5m.start()
+        IsOK5m = True
+
+    if (dateTime.minute % 15 == 0) and (IsOK15m == False): 
+        t15m = Thread(target=compute_process, args=["15m"])
+        t15m.start()
+        IsOK15m = True
+
+    if (dateTime.minute == 0) and (IsOK1h == False): 
+        t1h = Thread(target=compute_process, args=["1h"])
+        t1h.start()
+        IsOK1h= True
+
+    if (dateTime.hour in {0,4,8,12,16,20}) and (dateTime.minute == 0) and (IsOK4h == False): 
+        t4h = Thread(target=compute_process, args=["4h"])
+        t4h.start()
+        IsOK4h = True
+
+    if (dateTime.hour == 0) and (dateTime.minute == 0) and (IsOK1d == False): 
+        t1d = Thread(target=compute_process, args=["1d"])
+        t1d.start()
+        IsOK1d = True
+
+        try:
+            symbol_list = get_symbol_list('USDT','Future')
+        except Exception as e:
+            debugMsg = warn + " Error : \n" + str(e) + "\n\n"
+            debugMsg += warn + "\nSistem Tekrar Bağlanmayı Deniyor...\n" + warn
+            send_message(debugMsg)
+            sleep(5)
+            continue
+
+    if(t5m.is_alive() == True):
+        t5m.join()
+
+    if(t15m.is_alive() == True):
+        t15m.join()
+
+    if(t1h.is_alive() == True):
+        t1h.join()
+
+    if(t4h.is_alive() == True):
+        t4h.join()
+
+    if(t1d.is_alive() == True):
+        t1d.join()
+
+    time.sleep(1)
+
+
+### CODE BACKUPS
+'''
+IsTime15m = False
+IsTime1h = False
+IsTime4h = False
+IsTime1d = False
+
+dateTime = datetime.now()
+
+send_message(warn + "\nTarama Başlatıldı...\nSaat : " + str(dateTime.hour) + ":" + str(dateTime.minute) + "\n" + warn)
+symbol_list = get_symbol_list('USDT','Future')
+
+compute_process("5m")
 compute_process("15m")
 compute_process("1h")
 compute_process("4h")
@@ -136,15 +221,12 @@ while True:
         compute_process("4h")
         IsOK4h = True
 
-    if (dateTime.hour == 3) and (dateTime.minute == 0) and (IsOK1d == False): 
+    if (dateTime.hour == 0) and (dateTime.minute == 0) and (IsOK1d == False): 
         compute_process("1d")
         IsOK1d = True
 
     time.sleep(0.5)
-
-
-
-### CODE BACKUPS
+'''
 
 '''
 def job_15m():
