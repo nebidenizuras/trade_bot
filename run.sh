0d5707fbcff51e072c8f7e376a2e4a3bda778238
NDU <denizurasiu@gmail.com>
2025-07-12 13:05:59 +0300

    added sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+python3 -m venv venv
+source venv/bin/activate
+
+nohup python crypto_volume_strategy.py > crypto_output.log 2>&1 &
+nohup python bist_volume_strategy.py > bist_output.log 2>&1 &
+nohup python usstock_volume_strategy.py > usstock_output.log 2>&1 &
