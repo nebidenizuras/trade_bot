4f0e60601448f1232d71ac3584ec97514fd724aa
NDU <denizurasiu@gmail.com>
2025-08-26 23:00:55 +0300

    .

--- run.sh
+++ run.sh
@@ -7,3 +7,4 @@ source venv/bin/activate
 nohup python crypto_volume_strategy.py > crypto_output.log 2>&1 &
 nohup python bist_volume_strategy.py > bist_output.log 2>&1 &
 nohup python usstock_volume_strategy.py > usstock_output.log 2>&1 &
+nohup python crypto_volume_strategy_ema8.py > crypto2_output.log 2>&1 &
